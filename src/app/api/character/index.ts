f962ffba06c43265e4eb21fd4689a774e180feb7
smw0807 <smw0807@gmail.com>
2025-10-06 21:35:37 +0900

    feat: 캐릭터 기본 조회 api

--- src/app/api/character/index.ts
+++ src/app/api/character/index.ts
@@ -1,9 +1,31 @@
 import { _call } from '@/lib/nexon';
 // 캐릭터 식별자 조회
 export async function getOCID(characterName: string, server: string) {
-  const response = await _call('/maplestorym/v1/id', {
-    character_name: characterName,
-    world_name: server,
-  });
-  return response;
+  try {
+    const response = await _call('/maplestorym/v1/id', {
+      character_name: characterName,
+      world_name: server,
+    });
+    return response;
+  } catch (error) {
+    if (error instanceof Error && error.message.includes('400')) {
+      return null;
+    }
+    throw error;
+  }
+}
+
+// 기본 정보 조회
+export async function getCharacterBasic(ocid: string) {
+  try {
+    const response = await _call('/maplestorym/v1/character/basic', {
+      ocid,
+    });
+    return response;
+  } catch (error) {
+    if (error instanceof Error && error.message.includes('400')) {
+      return null;
+    }
+    throw error;
+  }
 }
